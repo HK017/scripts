67efad8a115501b4159bf225cbd4750940511d00
kai.hou <kai.hou@yhouse.com>
2019-04-14 20:23:35 +0800

    整理

--- python_start.py
+++ python_start.py
@@ -2465,3 +2465,36 @@ plt.ylabel('True positive rate')
 plt.title('ROC curve (zoomed in at top left)')
 plt.legend(loc='best')
 plt.show()
+
+
+import click
+@click.command()
+@click.option('--count', default = 1, help = 'Numbor of gretting')
+@click.option('--name', nargs=2, prompt = 'your name', help = 'the person to greet.', default = '')
+
+# @click.option('-m','--mail_to',default = '',help = 'mail to person email')
+# parameters introdutions
+# Click supports two types of parameters for scripts: options and arguments.
+# parameter type
+# If no type is provided, the type of the default value is used.
+# Multi Value Options
+
+#It automatically generates nicely formatted help pages:
+# prompt varible can advocate youself you name
+# default varible set per default value
+# help varible can print its help document
+# --name nargs=2 means name attribute have two varible
+def hello(count,name):
+    '''simple program that greets name for a total of count times'''
+    for i in range(int(count)):
+        click.echo('name1  is %s,name2 is %s' %  name)
+
+if __name__ == '__main__':
+    hello()
+
+# we havo to run in the command line,for example:
+# python env_click.py --help
+# python env_click.py --count=3 --name='houkai'
+# python env_click.py --count=3  after anter input you name via clue
+
+
